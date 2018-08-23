eec436b0cbfdb49e4a8cef6ca9b8b550a15dce42
Tehsurfer <37255664+Tehsurfer@users.noreply.github.com>
2018-08-23 12:18:58 +1200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -2,4 +2,4 @@ from service.app import app
 
 # For local server, access http://'your ip':80/
 if __name__ == '__main__':
-  app.run(debug=True, port=80)
+  app.run(debug=True, host='0.0.0.0', port=80)
