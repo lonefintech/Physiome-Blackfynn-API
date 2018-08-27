5d3cb29f9b31e0213c85d2fd5bfb82cc5b3c241a
Tehsurfer <37255664+Tehsurfer@users.noreply.github.com>
2018-08-27 13:15:54 +1200

    Update routes.py

--- backend/service/routes.py
+++ backend/service/routes.py
@@ -93,7 +93,7 @@ def channels():
 
 # /api/get_channel: Returns data for a single channel
 @app.route('/api/get_channel', methods=['GET'])
-def get_channel()
+def get_channel():
     name = request.headers['Name']
     requested_channel = request.headers['Channel']
     requested_channel = requested_channel.decode("utf-8")
