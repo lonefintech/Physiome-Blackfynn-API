3256a5153709b611df8c2a358d0663d0bf956ac1
Jesse Khorasanee <37255664+Tehsurfer@users.noreply.github.com>
2018-09-25 22:20:19 -0700

    Fixed typo

--- backend/server.py
+++ backend/server.py
@@ -3,5 +3,5 @@ from service.app import app
 # For local server, access http://'your ip':80/
 if __name__ == '__main__':
   app.run(ssl_context=('/etc/letsencrypt/live/blackfynnpythonlink.ml/fullchain.pem', 
-                       '/etc/letsencrypt/live/blackfynnpythonlink.ml/privkey.pem),
+                       '/etc/letsencrypt/live/blackfynnpythonlink.ml/privkey.pem'),
                        host='0.0.0.0', port=80)
