882a347197769ab5e84d4aa7b30b95c78641b892
Jesse Khorasanee <37255664+Tehsurfer@users.noreply.github.com>
2018-09-28 17:34:47 -0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -2,8 +2,6 @@ from service.app import app
 import ssl
 
 if __name__ == '__main__':
-  context = ssl.SSLContext(ssl.TLSv1_2_METHOD)
-  context.use_privatekey_file('/etc/letsencrypt/live/blackfynnpythonlink.ml/privkey.pem')
-  context.use_certificate_chain_file('/etc/letsencrypt/live/blackfynnpythonlink.ml/fullchain.pem')
-  context.use_certificate_file('/etc/letsencrypt/live/blackfynnpythonlink.ml/cert.pem')
+  context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+  context.load_cert_chain('/etc/letsencrypt/live/blackfynnpythonlink.ml/fullchain.pem', '/etc/letsencrypt/live/blackfynnpythonlink.ml/privkey.pem')
   app.run(host='0.0.0.0', port=80, threaded=True, ssl_context=context)
