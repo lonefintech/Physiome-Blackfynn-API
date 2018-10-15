7762bd0c6691f8c197331dfc24d398ffd3ddd8c5
Ubuntu <ubuntu@ip-172-31-34-245.us-east-2.compute.internal>
2018-10-15 00:51:35 +0000

    Added a Nginx reverse proxy

--- backend/server.py
+++ backend/server.py
@@ -4,4 +4,4 @@ import ssl
 if __name__ == '__main__':
   context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
   context.load_cert_chain('/etc/letsencrypt/live/blackfynnpythonlink.ml/fullchain.pem', '/etc/letsencrypt/live/blackfynnpythonlink.ml/privkey.pem')
-  app.run(host='0.0.0.0', port=80, threaded=True, ssl_context=context)
+  app.run(host='0.0.0.0', port=81, threaded=True)
