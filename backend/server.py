baf00cb59d931137c577ec49a4df07750b96aebd
root <root@ip-172-31-34-245.us-east-2.compute.internal>
2018-10-22 02:34:21 +0000

    modified port for server

--- backend/server.py
+++ backend/server.py
@@ -4,4 +4,4 @@ import ssl
 if __name__ == '__main__':
   context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
   context.load_cert_chain('/etc/letsencrypt/live/blackfynnpythonlink.ml/fullchain.pem', '/etc/letsencrypt/live/blackfynnpythonlink.ml/privkey.pem')
-  app.run(host='0.0.0.0', port=81, threaded=True)
+  app.run(host="127.0.0.1",port=82)
